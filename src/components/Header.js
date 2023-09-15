e95bd08e800519af03c933a45db791f2e4840fb5
Poorna Shri <poornashri2703@gmail.com>
2023-09-15 22:25:18 +0530

    Final update

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,8 @@ import React from "react";
 const Header = () =>{
     return(
         <div className="header">
+            {/* <h2>VIKRAM's</h2> */}
+            <h2>Mirthika's</h2>
             <h1>TO-DO-LIST</h1>
 
         </div>
